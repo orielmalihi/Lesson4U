6d2d83a62bbfa611f3a6a4156d542092691aa6b4
oriel <orielmalihi1@gmail.com>
2020-11-26 22:53:36 +0200

    loginActivity and registerActivity are working

--- app/src/main/java/com/example/lesson4u/MainActivity.java
+++ app/src/main/java/com/example/lesson4u/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,24 +19,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button studentb;
-    Button teacherb;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    Button loguot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        studentb = (Button)findViewById(R.id.studentButton);
-        teacherb = (Button)findViewById(R.id.teacherButton);
-        studentb.setOnClickListener(this);
-        teacherb.setOnClickListener(this);
+
+    }
+
+    protected void onStart() {
+        super.onStart();
+//        System.out.println("***********************"+auth.getUid());
+        if (auth.getUid() == null) {
+            Intent intent = new Intent(this, LoginOrRegister.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Already logged in", Toast.LENGTH_LONG).show();
+            loguot = findViewById(R.id.button3);
+            loguot.setOnClickListener(this);
+        }
+    }
+
 
 //        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("students/student1");
-        DatabaseReference myRootRef = database.getReference();
-//        myRef.setValue("oriel malihi");
-//        myRootRef.child("students").child("student").setValue("oriel");
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("students/student1");
+//        DatabaseReference myRootRef = database.getReference();
+//        myRef.setValue("ploni");
+//        myRootRef.child("students").child("student1").setValue("oriel");
 //        myRootRef.child("students").child("student2").setValue("yahav");
 //        myRootRef.child("students").child("student3").setValue("shoval");
 //        String s = myRef.getKey();
@@ -43,22 +57,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        Map<String, Object> map = new HashMap<>();
 //        map.put("student one", "student1");
 //        map.put("student two", "student2");
-//        myRootRef.child("students").updateChildren(map);
+//        myRootRef.child("students").child("student1").updateChildren(map);
 //        myRootRef.child("students").child("student3").removeValue();
-
-
-    }
+//
 
     @Override
     public void onClick(View v) {
-        if(v == studentb){
-            Intent intent = new Intent(this, loginOrRegister.class);
-            intent.putExtra("type", "student");
+        if(v == loguot){
+            auth.signOut();
+            Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
-        } else if(v == teacherb){
-            Intent intent = new Intent(this, loginOrRegister.class);
-            intent.putExtra("type", "teacher");
-            startActivity(intent);
+            finish();
         }
     }
 }
+
+
