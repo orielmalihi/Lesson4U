7e67a99cfcae149236178764139b703e24068ad7
shoval benayon <shovalbb14@gmail.com>
2020-12-08 00:34:38 +0200

    fixing SearchLessons

--- app/src/main/java/com/example/lesson4u/LessonResults.java
+++ app/src/main/java/com/example/lesson4u/LessonResults.java
@@ -1,9 +1,8 @@
 package com.example.lesson4u;
 
-import android.os.Bundle;
-
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class LessonResults extends AppCompatActivity {
 
@@ -11,6 +10,5 @@ public class LessonResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_results);
-
     }
 }
--- app/src/main/java/com/example/lesson4u/SearchForLessonsActivity.java
+++ app/src/main/java/com/example/lesson4u/SearchForLessonsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -28,6 +29,7 @@ import java.util.StringTokenizer;
 public class SearchForLessonsActivity extends AppCompatActivity {
 
     private FirebaseAuth auth = FirebaseAuth.getInstance();
+    final String TAG = "SearchLessonsActivity";
     private String currentuser;
     private Button bSearchLesson;
     private EditText eDate;
@@ -51,37 +53,41 @@ public class SearchForLessonsActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("lessons");
 
-        final ArrayList<LessonObj> MatchedLessons = new ArrayList<>();
-        final ArrayAdapter adapter = new ArrayAdapter<LessonObj>(this, R.layout.activity_search_for_lessons , MatchedLessons);
 
-
+        final ArrayList<String> MatchedLessons = new ArrayList<>();
+//        final ArrayAdapter adapter = new ArrayAdapter<LessonObj>(this, R.layout.activity_search_for_lessons , MatchedLessons);
+//
 
         bSearchLesson.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int beginst = Integer.parseInt(new StringTokenizer(beginTime.getText().toString(), ":").nextToken());
+                int endst = Integer.parseInt(new StringTokenizer(endTime.getText().toString(), ":").nextToken());
+                Log.d(TAG , "beginst =" + beginst + " endst = " + endst);
                 myRef.addValueEventListener(new ValueEventListener() {
 
                             @Override
                             public void onDataChange(DataSnapshot dataSnapshot) {
                                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                                     LessonObj tempLesson = snapshot.getValue(LessonObj.class);
-                                    if (tempLesson.getSubject().equals(spLessonSubjects.toString())) {
-                                        if (tempLesson.getLevel().equals(spLevel.toString())) {
-//                                            if (tempLesson.getDate().equals(eDate.getText().toString())) {
-//                                                int beginst = Integer.parseInt(new StringTokenizer(beginTime.toString(), ":").nextToken());
-//                                                int endst = Integer.parseInt(new StringTokenizer(endTime.toString(), ":").nextToken());
-//                                                int SchduledTime = Integer.parseInt(new StringTokenizer(tempLesson.getTime(), ":").nextToken());
-//                                                if (beginst <= SchduledTime && SchduledTime <= endst) {
-                                                    MatchedLessons.add(tempLesson);
-                                                    Toast.makeText(SearchForLessonsActivity.this, "Successfully ", Toast.LENGTH_SHORT).show();
+                                    if (!tempLesson.isScheduled()) {
+                                        int SchduledTime = Integer.parseInt(new StringTokenizer(tempLesson.getTime(), ":").nextToken());
+                                        if (tempLesson.getSubject().equals(spLessonSubjects.getSelectedItem().toString())) {
+                                            if (tempLesson.getLevel().equals(spLevel.getSelectedItem().toString())) {
+                                                if (tempLesson.getDate().equals(eDate.getText().toString())) {
+                                                    if (beginst <= SchduledTime && SchduledTime <= endst) {
+                                                        MatchedLessons.add(snapshot.getKey());
+                                                        Log.d(TAG, "key = " + snapshot.getKey());
+                                                        Toast.makeText(SearchForLessonsActivity.this, "Successfully ", Toast.LENGTH_SHORT).show();
 
-                                              // }
-                                           // }
+                                                    }
+                                                }
+                                            }
                                         }
                                     }
                                 }
-//                                Intent intent = new Intent(getApplicationContext(), LessonResults.class);
-//                                startActivity(intent);
+                                Intent intent = new Intent(getApplicationContext(), LessonResults.class);
+                                startActivity(intent);
                             }
                             @Override
                             public void onCancelled(DatabaseError databaseError) {
