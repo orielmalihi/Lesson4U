7e78bccb515619582c1b0031594a890b12413fea
shoval benayon <shovalbb14@gmail.com>
2020-12-11 11:29:02 +0200

    bug

--- app/src/main/java/com/example/lesson4u/LessonResults.java
+++ app/src/main/java/com/example/lesson4u/LessonResults.java
@@ -33,7 +33,7 @@ public class LessonResults extends AppCompatActivity {
         ArrayList<String> MatchedLessons = (ArrayList<String>) getIntent().getSerializableExtra("MatchedLessons");
         recyclerView = findViewById(R.id.recycle);
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        recyclerView.setAdapter();
+        //recyclerView.setAdapter();
 
 
     }
