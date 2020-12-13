bb5e4f59d3c18b9fea275eca58f09f5e70547ff7
shoval benayon <shovalbb14@gmail.com>
2020-12-13 11:49:24 +0200

    wrong

--- app/src/main/java/com/example/lesson4u/LessonResults.java
+++ app/src/main/java/com/example/lesson4u/LessonResults.java
@@ -1,23 +1,11 @@
 package com.example.lesson4u;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,10 +18,6 @@ import java.util.ArrayList;
 import java.util.ArrayList;
 
 public class LessonResults extends AppCompatActivity {
-    private FirebaseAuth auth = FirebaseAuth.getInstance();
-    RecyclerView recyclerView;
-    ArrayAdapter<String> arrayAdapter;
-
 
     //private ListView listView;
     final String TAG = "lessonResults";
@@ -46,16 +30,6 @@ public class LessonResults extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_results);
 
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("lessons");
-        ArrayList<String> MatchedLessons = (ArrayList<String>) getIntent().getSerializableExtra("MatchedLessons");
-        recyclerView = findViewById(R.id.recycle);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        //recyclerView.setAdapter();
-
-
-
         recyclerView = findViewById(R.id.recyclerView);
 
         Intent intent = getIntent();
@@ -66,7 +40,6 @@ public class LessonResults extends AppCompatActivity {
         MyAdapter myAdapter = new MyAdapter(this, MatchedLessons, lessonIDs);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
     }
 
 }
